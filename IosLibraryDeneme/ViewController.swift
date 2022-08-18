2b44913a9c85bc87a09f09842bb3584880f6473e
SahinKapan <sahinkapan@Sahins-MacBook-Pro.local>
2022-08-18 15:18:14 +0300

    Initial Commit

---
+++ IosLibraryDeneme/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  IosLibraryDeneme
+//
+//  Created by Sahin Kapan on 18.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        writeHello()    }
+    
+    func writeHello(){
+        print("Hello Virtual I")
+    }
+    
+
+
+}
+
